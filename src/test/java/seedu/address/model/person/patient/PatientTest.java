573697b7bd1a5d4cf6448c75f05b10b055b9e3cb
YangHeng Tang <yanghengtang@gmail.com>
2023-10-07 14:25:18 +0800

    Add PatientTest.java

---
+++ src/test/java/seedu/address/model/person/patient/PatientTest.java
@@ -0,0 +1,76 @@
+package seedu.address.model.person.patient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.address.testutil.TypicalPatient.ALICE;
+import static seedu.address.testutil.TypicalPatient.BOB;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.PatientBuilder;
+
+public class PatientTest {
+
+    @Test
+    public void isSamePatient() {
+        // same object -> returns true
+        assertTrue(ALICE.isSamePatient(ALICE));
+
+        // null -> returns false
+        assertFalse(ALICE.isSamePatient(null));
+
+        // same NRIC, all other attributes different -> returns true
+        Patient editedAlice = new PatientBuilder(ALICE).withPhone(VALID_PHONE_BOB).withName(VALID_NAME_BOB).build();
+        assertTrue(ALICE.isSamePatient(editedAlice));
+
+        // different NRIC, all other attributes same -> returns false
+        editedAlice = new PatientBuilder(ALICE).withNric(VALID_NRIC_BOB).build();
+        assertFalse(ALICE.isSamePatient(editedAlice));
+    }
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        Patient aliceCopy = new PatientBuilder(ALICE).build();
+        assertTrue(ALICE.equals(aliceCopy));
+
+        // same object -> returns true
+        assertTrue(ALICE.equals(ALICE));
+
+        // null -> returns false
+        assertFalse(ALICE.equals(null));
+
+        // different type -> returns false
+        assertFalse(ALICE.equals(5));
+
+        // different person -> returns false
+        assertFalse(ALICE.equals(BOB));
+
+        // different name -> returns false
+        Patient editedAlice = new PatientBuilder(ALICE).withName(VALID_NAME_BOB).build();
+        assertFalse(ALICE.equals(editedAlice));
+
+        // different phone -> returns false
+        editedAlice = new PatientBuilder(ALICE).withPhone(VALID_PHONE_BOB).build();
+        assertFalse(ALICE.equals(editedAlice));
+
+        // different name -> returns false
+        editedAlice = new PatientBuilder(ALICE).withName(VALID_NAME_BOB).build();
+        assertFalse(ALICE.equals(editedAlice));
+
+        // different nric -> returns false
+        editedAlice = new PatientBuilder(ALICE).withNric(VALID_NRIC_BOB).build();
+        assertFalse(ALICE.equals(editedAlice));
+    }
+
+    @Test
+    public void toStringMethod() {
+        String expected = Patient.class.getCanonicalName() + "{name=" + ALICE.getName() + ", phone=" + ALICE.getPhone()
+                + ", nric=" + ALICE.getNric() + "}";
+        assertEquals(expected, ALICE.toString());
+    }
+}
