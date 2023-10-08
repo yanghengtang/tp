65e273e708ee8b9218ef208ab8f89621457786fb
Derek Tan <derek.tan.jing.xu@gmail.com>
2023-10-08 15:42:14 +0800

    Add interface for all classes meant to be added to a UniqueList

---
+++ src/main/java/seedu/address/model/Listable.java
@@ -0,0 +1,19 @@
+package seedu.address.model;
+
+import seedu.address.model.person.Person;
+
+/**
+ * A interface for all classes that needs to be added to a UniqueList to implement.
+ */
+public interface Listable {
+    /**
+     * Checks if 2 Listables are the same. This method is separate from equals as it represents a weaker notion of
+     * equality.
+     *
+     * @param otherListable Another Listable to compare the current instance to.
+     * @return True if both listables are the same.
+     */
+    public boolean isSame(Listable otherListable);
+
+
+}
