4cf6a3267a71c051d4095ca977b9070f48763025
mounilsankar <mounil974@gmail.com>
2023-10-12 16:20:31 +0800

    Add AppointmentTime.java and update ParserUtil.java

---
+++ src/main/java/seedu/address/logic/parser/AddAppointmentCommandParser.java
@@ -0,0 +1,52 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.AddAppointmentCommand;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.appointment.Appointment;
+import seedu.address.model.person.*;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Parses input arguments and creates a new AddCommand object
+ */
+public class AddAppointmentCommandParser implements Parser<AddAppointmentCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddCommand
+     * and returns an AddCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddAppointmentCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_PATIENT_NRIC, PREFIX_DOCTOR_NRIC, PREFIX_APPOINTMENT_START_TIME, PREFIX_APPOINTMENT_END_TIME);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_PATIENT_NRIC, PREFIX_DOCTOR_NRIC, PREFIX_APPOINTMENT_START_TIME, PREFIX_APPOINTMENT_END_TIME)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddAppointmentCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_PATIENT_NRIC, PREFIX_DOCTOR_NRIC, PREFIX_APPOINTMENT_START_TIME, PREFIX_APPOINTMENT_END_TIME);
+        Nric patientNric = ParserUtil.parseNric(argMultimap.getValue(PREFIX_PATIENT_NRIC).get());
+        Nric doctorNric = ParserUtil.parseNric(argMultimap.getValue(PREFIX_DOCTOR_NRIC).get());
+
+        Appointment appointment = new Appointment(patientNric, doctorNric);
+
+        return new AddAppointmentCommand(appointment);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+}
