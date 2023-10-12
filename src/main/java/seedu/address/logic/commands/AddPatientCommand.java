3affdc30782d9615c32c4787d1a4ea35ad357809
Chee Teng <>
2023-10-13 01:01:22 +0800

    Add AddPatientCommand class

---
+++ src/main/java/seedu/address/logic/commands/AddPatientCommand.java
@@ -0,0 +1,73 @@
+package seedu.address.logic.commands;
+import seedu.address.model.NewModel;
+import seedu.address.model.person.patient.Patient;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+/**
+ * Adds a patient to the address book.
+ */
+public class AddPatientCommand extends NewCommand {
+
+    public static final String COMMAND_WORD = "add";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a patient to the address book. "
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + PREFIX_PHONE + "PHONE "
+            + PREFIX_NRIC + "NRIC "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "John Doe "
+            + PREFIX_PHONE + "98765432 "
+            + PREFIX_EMAIL + "A0123456B ";
+    public static final String MESSAGE_SUCCESS = "New patient added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This patient already exists in the address book";
+    private final Patient toAdd;
+
+    /**
+     * Creates an AddCommand to add the specified {@code Person}
+     */
+    public AddPatientCommand(Patient patient) {
+        requireNonNull(patient);
+        toAdd = patient;
+    }
+
+    @Override
+    public CommandResult execute(NewModel model) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasPatient(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
+
+        model.addPatient(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(toAdd)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof AddPatientCommand)) {
+            return false;
+        }
+
+        AddPatientCommand otherAddCommand = (AddPatientCommand) other;
+        return toAdd.equals(otherAddCommand.toAdd);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("toAdd", toAdd)
+                .toString();
+    }
+}
