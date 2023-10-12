a36d800d18450249dba46aa4e8068893fdf8d861
Chee Teng <>
2023-10-13 01:01:52 +0800

    Add NameContainsKeywordsPatientPredicate class

---
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPatientPredicate.java
@@ -0,0 +1,45 @@
+package seedu.address.model.person;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.person.patient.Patient;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ */
+public class NameContainsKeywordsPatientPredicate implements Predicate<Patient> {
+    private final List<String> keywords;
+
+    public NameContainsKeywordsPatientPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Patient patient) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(patient.getName().fullName, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof NameContainsKeywordsPatientPredicate)) {
+            return false;
+        }
+
+        NameContainsKeywordsPatientPredicate otherNameContainsKeywordsPatientPredicate = (NameContainsKeywordsPatientPredicate) other;
+        return keywords.equals(otherNameContainsKeywordsPatientPredicate.keywords);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("keywords", keywords).toString();
+    }
+}
