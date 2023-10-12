221778798fa6ad4fd92efb9cd2bb974699094968
Chee Teng <>
2023-10-13 05:53:16 +0800

    Fix Checkstyle

--- src/main/java/seedu/address/logic/parser/AddPatientCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddPatientCommandParser.java
@@ -5,7 +5,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NRIC;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 
-
 import java.util.stream.Stream;
 
 import seedu.address.logic.commands.AddPatientCommand;
