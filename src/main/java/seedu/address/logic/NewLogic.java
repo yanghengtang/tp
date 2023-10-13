74531bb7ae2800494eb7ed1edd805793a5db54c2
mounilsankar <mounil974@gmail.com>
2023-10-13 17:26:51 +0800

    Add NewAddressBookParser.java, NewLogic.java, NewLogicManager.java, New Parser.java, NewModelManager.java

---
+++ src/main/java/seedu/address/logic/NewLogic.java
@@ -0,0 +1,60 @@
+package seedu.address.logic;
+
+import java.nio.file.Path;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyDatabase;
+import seedu.address.model.appointment.Appointment;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.doctor.Doctor;
+import seedu.address.model.person.patient.Patient;
+
+/**
+ * API of the Logic component
+ */
+public interface NewLogic {
+    /**
+     * Executes the command and returns the result.
+     * @param commandText The command as entered by the user.
+     * @return the result of the command execution.
+     * @throws CommandException If an error occurs during command execution.
+     * @throws ParseException If an error occurs during parsing.
+     */
+    CommandResult execute(String commandText) throws CommandException, ParseException;
+
+    /**
+     * Returns the database.
+     *
+     * @see seedu.address.model.NewModel#getDatabase()
+     */
+    ReadOnlyDatabase getDatabase();
+
+    /** Returns an unmodifiable view of the filtered list of Appointments */
+    ObservableList<Appointment> getFilteredAppointmentList();
+
+    /** Returns an unmodifiable view of the filtered list of Patients */
+    ObservableList<Patient> getFilteredPatientList();
+
+    /** Returns an unmodifiable view of the filtered list of Doctors */
+    ObservableList<Doctor> getFilteredDoctorList();
+
+    /**
+     * Returns the user prefs' address book file path.
+     */
+    Path getDatabaseFilePath();
+
+    /**
+     * Returns the user prefs' GUI settings.
+     */
+    GuiSettings getGuiSettings();
+
+    /**
+     * Set the user prefs' GUI settings.
+     */
+    void setGuiSettings(GuiSettings guiSettings);
+}
---
+++ src/main/java/seedu/address/logic/parser/NewParser.java
@@ -0,0 +1,17 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.NewCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Represents a Parser that is able to parse user input into a {@code Command} of type {@code T}.
+ */
+public interface NewParser<T extends NewCommand> {
+
+    /**
+     * Parses {@code userInput} into a command and returns it.
+     * @throws ParseException if {@code userInput} does not conform the expected format
+     */
+    T parse(String userInput) throws ParseException;
+}
