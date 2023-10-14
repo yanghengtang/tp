c2f0bc6d49859c8995df5b0bee68c09126354734
YangHeng Tang <yanghengtang@gmail.com>
2023-10-14 23:07:35 +0800

    Add AppointmentCard.java

---
+++ src/main/java/seedu/address/ui/AppointmentCard.java
@@ -0,0 +1,52 @@
+package seedu.address.ui;
+
+import java.time.format.DateTimeFormatter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.appointment.Appointment;
+
+/**
+ * An UI component that displays information of a {@code Patient}.
+ */
+public class AppointmentCard extends UiPart<Region> {
+    private static final String FXML = "AppointmentListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Appointment appointment;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label startDateTime;
+    @FXML
+    private Label endDateTime;
+    @FXML
+    private Label id;
+    @FXML
+    private Label patientNric;
+    @FXML
+    private Label doctorNric;
+
+    /**
+     * Creates a {@code PatientCode} with the given {@code Patient} and index to display.
+     */
+    public AppointmentCard(Appointment appointment, int displayedIndex) {
+        super(FXML);
+        this.appointment = appointment;
+        id.setText(displayedIndex + ". ");
+        startDateTime.setText(appointment.getStartTime().time.format(DateTimeFormatter.ofPattern("MMM d yyyy HH:mm")));
+        endDateTime.setText(appointment.getEndTime().time.format(DateTimeFormatter.ofPattern("MMM d yyyy HH:mm")));
+        patientNric.setText(appointment.getPatientNric().nric);
+        doctorNric.setText(appointment.getDoctorNric().nric);
+    }
+}
