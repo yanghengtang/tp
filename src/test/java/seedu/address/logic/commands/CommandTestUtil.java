0e2a7c9ba83334c1e91f5389c8f69b5bf3693202
Chee Teng <>
2023-10-17 04:19:11 +0800

    Revert "Create test for ListPatientsCommand Class"
    This reverts commit 432c1c8fc59ca7d78ba781984b046e108d805d18.

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -25,7 +25,6 @@ import seedu.address.model.Database;
 import seedu.address.model.Model;
 import seedu.address.model.NewModel;
 import seedu.address.model.appointment.Appointment;
-import seedu.address.model.person.NameContainsKeywordsPatientPredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.doctor.Doctor;
@@ -208,20 +207,6 @@ public class CommandTestUtil {
         model.updateFilteredPersonList(new NameContainsKeywordsPredicate(Arrays.asList(splitName[0])));
 
         assertEquals(1, model.getFilteredPersonList().size());
-    }
-
-    /**
-     * Updates {@code model}'s filtered list to show only the patient at the given {@code targetIndex} in the
-     * {@code model}'s database.
-     */
-    public static void showPatientAtIndex(NewModel model, Index targetIndex) {
-        assertTrue(targetIndex.getZeroBased() < model.getFilteredPatientList().size());
-
-        Patient patient = model.getFilteredPatientList().get(targetIndex.getZeroBased());
-        final String[] splitName = patient.getName().fullName.split("\\s+");
-        model.updateFilteredPatientList(new NameContainsKeywordsPatientPredicate(Arrays.asList(splitName[0])));
-
-        assertEquals(1, model.getFilteredPatientList().size());
     }
 
 }
