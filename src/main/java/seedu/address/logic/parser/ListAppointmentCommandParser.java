6e44874a30dd78c58e46d631ee9825c3df94661d
mounilsankar <mounil974@gmail.com>
2023-10-17 22:48:12 +0800

    Add ListAppointmentCommandParser

--- src/main/java/seedu/address/logic/parser/ListAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListAppointmentCommandParser.java
@@ -1,13 +1,5 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT_END_TIME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT_START_TIME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DOCTOR_NRIC;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PATIENT_NRIC;
-
-import java.util.stream.Stream;
-
 import seedu.address.logic.commands.AddAppointmentCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.appointment.Appointment;
@@ -15,21 +7,23 @@ import seedu.address.model.appointment.AppointmentEndTime;
 import seedu.address.model.appointment.AppointmentStartTime;
 import seedu.address.model.person.Nric;
 
-/**
- * Parses input arguments and creates a new AddCommand object
- */
-public class AddAppointmentCommandParser implements NewParser<AddAppointmentCommand> {
+import java.util.stream.Stream;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT_END_TIME;
+
+public class ListAppointmentCommandParser {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the AddAppointmentCommand
-     * and returns an AddAppointmentCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the ListAppointmentCommand
+     * and returns an ListAppointmentCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddAppointmentCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
-                        PREFIX_PATIENT_NRIC,
-                        PREFIX_DOCTOR_NRIC,
-                        PREFIX_APPOINTMENT_START_TIME, PREFIX_APPOINTMENT_END_TIME);
+                PREFIX_PATIENT_NRIC,
+                PREFIX_DOCTOR_NRIC);
 
         if (!arePrefixesPresent(argMultimap,
                 PREFIX_PATIENT_NRIC,
@@ -42,9 +36,10 @@ public class AddAppointmentCommandParser implements NewParser<AddAppointmentComm
         }
 
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_PATIENT_NRIC,
-                PREFIX_DOCTOR_NRIC,
-                PREFIX_APPOINTMENT_START_TIME,
-                PREFIX_APPOINTMENT_END_TIME);
+                PREFIX_DOCTOR_NRIC);
+        if (!arePrefixesPresent(argMultimap, PREFIX_PATIENT_NRIC)) {
+
+        }
         Nric patientNric = ParserUtil.parseNric(argMultimap.getValue(PREFIX_PATIENT_NRIC).get());
         Nric doctorNric = ParserUtil.parseNric(argMultimap.getValue(PREFIX_DOCTOR_NRIC).get());
         AppointmentStartTime start = ParserUtil
