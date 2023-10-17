7a67643b23e15b584ea1741dab7b9e3b3b05d3d2
mounilsankar <mounil974@gmail.com>
2023-10-17 21:28:07 +0800

    Implement Appointment predicate tests

---
+++ src/test/java/seedu/address/model/appointment/AppointmentEqualDoctorNricPredicateTest.java
@@ -0,0 +1,85 @@
+package seedu.address.model.appointment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalPatient.ALICE_NRIC;
+import static seedu.address.testutil.TypicalPatient.BENSON_NRIC;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.person.Nric;
+import seedu.address.testutil.AppointmentBuilder;
+
+public class AppointmentEqualDoctorNricPredicateTest {
+
+    @Test
+    public void equals() {
+        Nric firstDoctorNric = new Nric(ALICE_NRIC);
+        Nric secondDoctorNric = new Nric(BENSON_NRIC);;
+
+        AppointmentEqualDoctorNricPredicate firstPredicate =
+                new AppointmentEqualDoctorNricPredicate(firstDoctorNric);
+        AppointmentEqualDoctorNricPredicate secondPredicate =
+                new AppointmentEqualDoctorNricPredicate(secondDoctorNric);
+
+        // same object -> returns true
+        assertTrue(firstPredicate.equals(firstPredicate));
+
+        // same values -> returns true
+        AppointmentEqualDoctorNricPredicate firstPredicateCopy =
+                new AppointmentEqualDoctorNricPredicate(firstDoctorNric);
+        assertTrue(firstPredicate.equals(firstPredicateCopy));
+
+        // different types -> returns false
+        assertFalse(firstPredicate.equals(1));
+
+        // null -> returns false
+        assertFalse(firstPredicate.equals(null));
+
+        // different doctor -> returns false
+        assertFalse(firstPredicate.equals(secondPredicate));
+    }
+
+    @Test
+    public void test_appointmentEqualDoctorNric_returnsTrue() {
+        // Matching DoctorNric
+        AppointmentEqualDoctorNricPredicate predicate =
+                new AppointmentEqualDoctorNricPredicate(new Nric(ALICE_NRIC));
+        assertTrue(predicate.test(new AppointmentBuilder()
+                .withDoctorNric(ALICE_NRIC)
+                .build()));
+
+        // Mixed case Nric
+        predicate = new AppointmentEqualDoctorNricPredicate(new Nric("t0123456g"));
+        assertTrue(predicate.test(new AppointmentBuilder()
+                .withDoctorNric("T0123456G")
+                .build()));
+    }
+
+    @Test
+    public void test_appointmentDoesNotHaveEqualDoctorNric_returnsFalse() {
+        // Non matching nric
+        AppointmentEqualDoctorNricPredicate predicate =
+                new AppointmentEqualDoctorNricPredicate(new Nric(ALICE_NRIC));
+        assertFalse(predicate.test(new AppointmentBuilder()
+                .withDoctorNric(BENSON_NRIC)
+                .build()));
+
+        // nric match patient nric, but does not doctor nric
+        predicate = new AppointmentEqualDoctorNricPredicate(new Nric(ALICE_NRIC));
+        assertFalse(predicate.test(new AppointmentBuilder()
+                .withDoctorNric(BENSON_NRIC)
+                .withPatientNric(ALICE_NRIC)
+                .build()));
+    }
+
+    @Test
+    public void toStringMethod() {
+        Nric nric = new Nric(ALICE_NRIC);
+        AppointmentEqualDoctorNricPredicate predicate = new AppointmentEqualDoctorNricPredicate(nric);
+
+        String expected = AppointmentEqualDoctorNricPredicate.class.getCanonicalName() + "{nric=" + nric + "}";
+        assertEquals(expected, predicate.toString());
+    }
+}
---
+++ src/test/java/seedu/address/model/appointment/AppointmentEqualPatientNricPredicateTest.java
@@ -0,0 +1,85 @@
+package seedu.address.model.appointment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalPatient.ALICE_NRIC;
+import static seedu.address.testutil.TypicalPatient.BENSON_NRIC;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.person.Nric;
+import seedu.address.testutil.AppointmentBuilder;
+
+public class AppointmentEqualPatientNricPredicateTest {
+
+    @Test
+    public void equals() {
+        Nric firstPatientNric = new Nric(ALICE_NRIC);
+        Nric secondPatientNric = new Nric(BENSON_NRIC);;
+
+        AppointmentEqualPatientNricPredicate firstPredicate =
+                new AppointmentEqualPatientNricPredicate(firstPatientNric);
+        AppointmentEqualPatientNricPredicate secondPredicate =
+                new AppointmentEqualPatientNricPredicate(secondPatientNric);
+
+        // same object -> returns true
+        assertTrue(firstPredicate.equals(firstPredicate));
+
+        // same values -> returns true
+        AppointmentEqualPatientNricPredicate firstPredicateCopy =
+                new AppointmentEqualPatientNricPredicate(firstPatientNric);
+        assertTrue(firstPredicate.equals(firstPredicateCopy));
+
+        // different types -> returns false
+        assertFalse(firstPredicate.equals(1));
+
+        // null -> returns false
+        assertFalse(firstPredicate.equals(null));
+
+        // different doctor -> returns false
+        assertFalse(firstPredicate.equals(secondPredicate));
+    }
+
+    @Test
+    public void test_appointmentEqualPatientNric_returnsTrue() {
+        // Matching Patient Nric
+        AppointmentEqualPatientNricPredicate predicate =
+                new AppointmentEqualPatientNricPredicate(new Nric(ALICE_NRIC));
+        assertTrue(predicate.test(new AppointmentBuilder()
+                .withPatientNric(ALICE_NRIC)
+                .build()));
+
+        // Mixed case Nric
+        predicate = new AppointmentEqualPatientNricPredicate(new Nric("t0123456g"));
+        assertTrue(predicate.test(new AppointmentBuilder()
+                .withPatientNric("T0123456G")
+                .build()));
+    }
+
+    @Test
+    public void test_appointmentDoesNotHaveEqualPatientNric_returnsFalse() {
+        // Non matching nric
+        AppointmentEqualPatientNricPredicate predicate =
+                new AppointmentEqualPatientNricPredicate(new Nric(ALICE_NRIC));
+        assertFalse(predicate.test(new AppointmentBuilder()
+                .withPatientNric(BENSON_NRIC)
+                .build()));
+
+        // nric match doctor nric, but does not matching patient nric
+        predicate = new AppointmentEqualPatientNricPredicate(new Nric(ALICE_NRIC));
+        assertFalse(predicate.test(new AppointmentBuilder()
+                .withPatientNric(BENSON_NRIC)
+                .withDoctorNric(ALICE_NRIC)
+                .build()));
+    }
+
+    @Test
+    public void toStringMethod() {
+        Nric nric = new Nric(ALICE_NRIC);
+        AppointmentEqualPatientNricPredicate predicate = new AppointmentEqualPatientNricPredicate(nric);
+
+        String expected = AppointmentEqualPatientNricPredicate.class.getCanonicalName() + "{nric=" + nric + "}";
+        assertEquals(expected, predicate.toString());
+    }
+}
