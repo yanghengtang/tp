2bbb030110807c7e3b0aadc0dccfd4166abfc65d
YangHeng Tang <yanghengtang@gmail.com>
2023-10-20 18:56:26 +0800

    Update TestUtil.java

---
+++ src/test/java/seedu/address/testutil/TestUtil.java
@@ -0,0 +1,55 @@
+package seedu.address.testutil;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.NewModel;
+import seedu.address.model.person.patient.Patient;
+
+/**
+ * A utility class for test cases.
+ */
+public class TestUtil {
+
+    /**
+     * Folder used for temp files created during testing. Ignored by Git.
+     */
+    private static final Path SANDBOX_FOLDER = Paths.get("src", "test", "data", "sandbox");
+
+    /**
+     * Appends {@code fileName} to the sandbox folder path and returns the resulting path.
+     * Creates the sandbox folder if it doesn't exist.
+     */
+    public static Path getFilePathInSandboxFolder(String fileName) {
+        try {
+            Files.createDirectories(SANDBOX_FOLDER);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return SANDBOX_FOLDER.resolve(fileName);
+    }
+
+    /**
+     * Returns the middle index of the person in the {@code model}'s patient list.
+     */
+    public static Index getMidIndex(NewModel model) {
+        return Index.fromOneBased(model.getFilteredPatientList().size() / 2);
+    }
+
+    /**
+     * Returns the last index of the person in the {@code model}'s patient list.
+     */
+    public static Index getLastIndex(NewModel model) {
+        return Index.fromOneBased(model.getFilteredPatientList().size());
+    }
+
+    /**
+     * Returns the person in the {@code model}'s person list at {@code index}.
+     */
+    public static Patient getPatient(NewModel model, Index index) {
+        return model.getFilteredPatientList().get(index.getZeroBased());
+    }
+}
