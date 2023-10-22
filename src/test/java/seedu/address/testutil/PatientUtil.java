349a01fd3ef7d82af8bf453068c8cf7e918fbdcd
mounilsankar <mounil974@gmail.com>
2023-10-23 01:26:53 +0800

    Add tests for EditPatientCommand in MediConnectParserTest

--- src/test/java/seedu/address/testutil/PatientUtil.java
+++ src/test/java/seedu/address/testutil/PatientUtil.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NRIC;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 
 import seedu.address.logic.commands.AddPatientCommand;
+import seedu.address.logic.commands.EditPatientCommand.EditPatientDescriptor;
 import seedu.address.model.person.patient.Patient;
 
 /**
@@ -30,4 +31,14 @@ public class PatientUtil {
         return sb.toString();
     }
 
+    /**
+     * Returns the part of command string for the given {@code descriptor}'s details.
+     */
+    public static String getEditPatientDescriptorDetails(EditPatientDescriptor descriptor) {
+        StringBuilder sb = new StringBuilder();
+        descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullName).append(" "));
+        descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
+        descriptor.getNric().ifPresent(nric -> sb.append(PREFIX_NRIC).append(nric.nric).append(" "));
+        return sb.toString();
+    }
 }
