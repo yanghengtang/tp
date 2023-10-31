2359b9cfe62c9db0c13b08abc06298b81d15eafc
mounilsankar <mounil974@gmail.com>
2023-10-31 09:57:10 +0800

    Update MediConnectParser

--- src/main/java/seedu/address/logic/parser/MediConnectParser.java
+++ src/main/java/seedu/address/logic/parser/MediConnectParser.java
@@ -11,6 +11,7 @@ import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.AddAppointmentCommand;
 import seedu.address.logic.commands.AddDoctorCommand;
 import seedu.address.logic.commands.AddPatientCommand;
+import seedu.address.logic.commands.AddPrescriptionCommand;
 import seedu.address.logic.commands.AppointmentRemarkCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteAppointmentCommand;
@@ -117,6 +118,9 @@ public class MediConnectParser {
 
         case AppointmentRemarkCommand.COMMAND_WORD:
             return new AppointmentRemarkCommandParser().parse(arguments);
+
+        case AddPrescriptionCommand.COMMAND_WORD:
+            return (new AddPrescriptionCommandParser().parse(arguments));
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
