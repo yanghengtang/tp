cd7351aa996b16d033cefc82af97e881eb90f545
kimshitong <shitongkim28@gmail.com>
2023-10-31 13:35:34 +0800

    Modify MediConnectParser with addition of addDoctorSpecialisationCommand

--- src/main/java/seedu/address/logic/parser/MediConnectParser.java
+++ src/main/java/seedu/address/logic/parser/MediConnectParser.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.AddAppointmentCommand;
 import seedu.address.logic.commands.AddDoctorCommand;
+import seedu.address.logic.commands.AddDoctorSpecialisationCommand;
 import seedu.address.logic.commands.AddPatientCommand;
 import seedu.address.logic.commands.AppointmentRemarkCommand;
 import seedu.address.logic.commands.Command;
@@ -121,6 +122,9 @@ public class MediConnectParser {
 
         case PatientRemarkCommand.COMMAND_WORD:
             return new PatientRemarkCommandParser().parse(arguments);
+
+        case AddDoctorSpecialisationCommand.COMMAND_WORD:
+            return new AddDoctorTagCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
