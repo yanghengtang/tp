899fa57b645c804e787360f3339da1679d002831
Chee Teng <>
2023-10-31 20:21:12 +0800

    Refactor DeletePatientTagCommandParser to DeleteMedicalConditionCommandParser

--- src/main/java/seedu/address/logic/parser/DeleteMedicalConditionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteMedicalConditionCommandParser.java
@@ -6,21 +6,21 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.DeletePatientTagCommand;
+import seedu.address.logic.commands.DeleteMedicalConditionCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.tag.Tag;
 
 /**
- * Parses input arguments and creates a new DeletePatientTagCommand object
+ * Parses input arguments and creates a new DeleteMedicalConditionCommand object
  */
-public class DeletePatientTagCommandParser implements Parser<DeletePatientTagCommand> {
+public class DeleteMedicalConditionCommandParser implements Parser<DeleteMedicalConditionCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeletePatientTagCommand
-     * and returns a DeletePatientTagCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the DeleteMedicalConditionCommand
+     * and returns a DeleteMedicalConditionCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public DeletePatientTagCommand parse(String args) throws ParseException {
+    public DeleteMedicalConditionCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_TAG);
 
@@ -30,19 +30,19 @@ public class DeletePatientTagCommandParser implements Parser<DeletePatientTagCom
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    DeletePatientTagCommand.MESSAGE_USAGE),
+                    DeleteMedicalConditionCommand.MESSAGE_USAGE),
                     pe);
         }
 
         if (!arePrefixesPresent(argMultimap, PREFIX_TAG)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    DeletePatientTagCommand.MESSAGE_USAGE));
+                    DeleteMedicalConditionCommand.MESSAGE_USAGE));
         }
 
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_TAG);
         Tag condition = ParserUtil.parseTag(argMultimap.getValue(PREFIX_TAG).get());
 
-        return new DeletePatientTagCommand(index, condition);
+        return new DeleteMedicalConditionCommand(index, condition);
     }
 
     /**
