3bdef2119fcd16aed88e7c08927d3865fae40e33
Derek Tan <derek.tan.jing.xu@gmail.com>
2023-11-01 21:46:55 +0800

    Add javadocs

--- src/main/java/seedu/address/logic/commands/ExitCommand.java
+++ src/main/java/seedu/address/logic/commands/ExitCommand.java
@@ -3,6 +3,9 @@ package seedu.address.logic.commands;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
+/**
+ * Terminates the program.
+ */
 public class ExitCommand extends Command {
     public static final String COMMAND_WORD = "exit";
 
