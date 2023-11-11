f7b33309f1c411570e4c3dfb652355384ed48913
YangHeng Tang <yanghengtang@gmail.com>
2023-11-12 00:37:31 +0800

    Fix error message in EditDoctorCommand

--- src/main/java/seedu/address/logic/commands/EditDoctorCommand.java
+++ src/main/java/seedu/address/logic/commands/EditDoctorCommand.java
@@ -31,7 +31,7 @@ public class EditDoctorCommand extends Command {
             + "Existing values will be overwritten by the input values.\n"
             + "Parameters: INDEX (must be a positive integer) "
             + "[" + PREFIX_NAME + "NAME] "
-            + "[" + PREFIX_NRIC + "NRIC]...\n"
+            + "[" + PREFIX_NRIC + "NRIC]\n"
             + "Example: " + COMMAND_WORD + " 1 "
             + PREFIX_NAME + "BOBE "
             + PREFIX_NRIC + "T5012345A";
